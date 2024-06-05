2da6e1cccc5f3ffb9ccb07debce65a6d46d230e5
Gabriel Santiago <68571524+Mordisquinha@users.noreply.github.com>
2024-06-05 01:57:37 -0300

    create_operations (#2)
    1. Create Table
    2. Create all commands sql

--- src/databases/sqlite/infra/tables/musicGallery/columns.py
+++ src/databases/sqlite/infra/tables/musicGallery/columns.py
@@ -2,7 +2,6 @@ from cerberus import Validator
 
 schema = Validator(
     {
-        'music_id': {'type': 'integer'},
         'music_name': {'type': 'string'},
         'music_publish_year': {'type': 'integer'}
     }
